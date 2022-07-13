ac04d8e8ac9a41520ff15e06df06387bb28511de
fartuney <102562065+fartuney@users.noreply.github.com>
2022-07-12 18:47:49 -0600

    Added Dictionary API integration

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,24 @@
 import  logo from "./logo.png"
 import './App.css';
+import Dictionary from "./Dictionary";
 
   export default function App() {
   return (
     <div className="App">
+      <div className = "container">
       <header className="App-header">
    < img src ={logo}className ="App-logo img-fluid" alt = "logo" 
  />
-      </header>
+ </header>
+ <main>
+  <Dictionary />
+ </main>
+ <footer className = "App-footer">  
+ <small>
+ Coded by Fartuney 
+ </small>
+ </footer>
+    </div>
     </div>
   );
 }
