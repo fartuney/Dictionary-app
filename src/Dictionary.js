faeb9173392e4c2987e491cac5aba3966323f3f7
fartuney <102562065+fartuney@users.noreply.github.com>
2022-07-19 22:18:40 -0600

    added API key

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -7,14 +7,22 @@ export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
 let [results, setResults]= useState(null);
 let [ loaded, setLoaded ]= useState(false);
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     console.log(response.data[0]);
     setResults(response.data[0]);
+   }
+   function handlePexelsResponse(response){
+console.log(response)
    }
  function search(){
    // documentation: https://dictionaryapi.dev/
    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-   axios.get(apiUrl).then(handleResponse);
+   axios.get(apiUrl).then(handleDictionaryResponse);
+
+   let pexelsApiKey ="563492ad6f91700001000001410cd53c85f04486ab4e10023736776a"
+   let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=1`;
+  axios.get(pexelsApiUrl,   { headers: {"Authorization" : `Bearer ${pexelsApiKey}`} }).then(handlePexelsResponse);
+
  }
 
   function handleSubmit(event) {
